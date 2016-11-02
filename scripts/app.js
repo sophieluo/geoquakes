d6e8fed05cf97fa4dc7df85cdd91af3862b8422f
sophieluo <sophie@groobusiness.com>
2016-11-02 12:13:48 -0700

    used handlebars to display quake title from json

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 // define globals
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+
 $(document).on("ready", function() {
 
   $.ajax({
@@ -11,16 +12,20 @@ $(document).on("ready", function() {
   });
 
 function onSuccess(json) {
-    console.log(json);// for (var i=0;i<json.data.length;i++) {
-    //   var successGif = json.data[i].images.fixed_height.url;
-    //   $(".gif-gallery").append("<img src='"+successGif+"'>");
-  // }
+    // console.log(json);
+  //   for (var i=0;i<json.features.length;i++) {
+  //      var quakeTitle = json.features[i].properties.title;
+  //      $("#info").append("<p>"+quakeTitle+"</p");
+  //  }
+   var source= $('#earthquake-li-template').html();
+   var template = Handlebars.compile(source);
+   var quakeHtml = template({ earthquakes:json.features });
+  //  console.log(quakeHtml)
+    $("#earthquakes-list").append(quakeHtml);
 }
 
   function onError(xhr, status, errorThrown) {
       alert("Sorry, there was a problem!");
-      // console.log("Error: " + errorThrown);
-      // console.log("Status: " + status);
-      // console.dir(xhr);
   }
+
 });
